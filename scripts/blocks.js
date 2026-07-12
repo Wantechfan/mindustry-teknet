1d59309544f76805834d4f356521fc3063965200
WantechFan <alifiandratsaqifp@gmail.com>
2026-07-12 17:26:45 +0700

    Blyad

--- scripts/blocks.js
+++ scripts/blocks.js
@@ -4,10 +4,10 @@ const coreMulet = extend(CoreBlock, "coreMulet", {
     itemCapacity: 3000,
     unitType: UnitTypes.eclipse, // Change later
 
-    load() {
+    load: function() {
         this.super$load();
-        this.region = Core.atlas.find(this.mod.name + "-effect-" + this.name);
-        this.teamRegion = Core.atlas.find(this.mod.name + "-effect-" + this.name + "-team");
+        this.region = Core.atlas.find(this.name);
+        this.teamRegion = Core.atlas.find(this.name + "-team");
     }
 });
 
