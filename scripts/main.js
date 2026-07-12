795a71009054d81fe70f5d3747c07629c13360e2
WantechFan <alifiandratsaqifp@gmail.com>
2026-07-12 23:51:31 +0700

    More log.info

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,8 @@ teknet.subtitle = Core.bundle.get("mod.teknet.subtitle");
 Log.info("It works before require");
 try {
     require("blocks");
+    Log.info("It works when require");
 } catch(e) {
-    Log.err("Error while requiring blocks: " + e);
+    Log.err("Had seizures while searching for blocks that doesn't exist: " + e);
 }
 Log.info("It works after require");
