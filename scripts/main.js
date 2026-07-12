5b8e42dc5dded60502d9ba0784fb677358bb0e23
WantechFan <alifiandratsaqifp@gmail.com>
2026-07-12 14:47:09 +0700

    Fah

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-
+const coreMulet = extend(CoreBlock, "coreMulet", {
+    size: 3,
+    health: 4000,
+    itemCapacity: 10000,
+    unitType: UnitTypes.eclipse // Change this later
+    requirements: ItemStack.with(
+        // Change this later with new resources
+        Items.surgeAlloy, 1
+    ),
+    
+    category: Category.effect
+});
