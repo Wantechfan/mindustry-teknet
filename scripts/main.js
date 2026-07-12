76e457d26b4cabed50251bbc68443b827e32aac3
WantechFan <alifiandratsaqifp@gmail.com>
2026-07-12 23:50:27 +0700

    Even further bug tracking

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,9 @@ teknet.author = Core.bundle.get("mod.teknet.author");
 teknet.description = Core.bundle.get("mod.teknet.description");
 teknet.subtitle = Core.bundle.get("mod.teknet.subtitle");
 Log.info("It works before require");
-require("blocks");
+try {
+    require("blocks");
+} catch(e) {
+    Log.err("Error while requiring blocks: " + e);
+}
 Log.info("It works after require");
