16763e1e5f609e6564e601172b619d120faa779f
WantechFan <alifiandratsaqifp@gmail.com>
2026-07-12 23:35:22 +0700

    Add logging around 'blocks' module requirement
    Added logging before and after requiring the 'blocks' module.

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
-require("blocks")
 const teknet = Vars.mods.locateMod("teknet").meta;
 
 teknet.displayName = Core.bundle.get("mod.teknet.name");
 teknet.author = Core.bundle.get("mod.teknet.author");
 teknet.description = Core.bundle.get("mod.teknet.description");
 teknet.subtitle = Core.bundle.get("mod.teknet.subtitle");
+Log.info("It works before require")
+require("blocks")
+Log.info("It works after require")
