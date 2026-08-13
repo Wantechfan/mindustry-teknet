ebc237304f5baece38103e1c9696a9f3ea16d0b4
WantechFan <alifiandratsaqifp@gmail.com>
2026-08-13 16:00:43 +0700

    Fuahh

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,19 @@ teknet.displayName = Core.bundle.get("mod.teknet.name");
 teknet.author = Core.bundle.get("mod.teknet.author");
 teknet.description = Core.bundle.get("mod.teknet.description");
 teknet.subtitle = Core.bundle.get("mod.teknet.subtitle");
+
 try {
     require("blocks");
     require("units");
 } catch(e) {
-    function getRandomString(Arr) {
-        const randomIndex = Math.floor(Math.random() * Arr.length);
+    function getRandomString(arr) {
+        const randomIndex = Math.floor(Math.random() * arr.length);
         return arr[randomIndex];
     }
     let strrandom = [
         "Had seizures while searching for blocks that doesn't exist",
-        "But for some reason I did a failure"]
+        "But for some reason I did a failure"
+    ];
     let shit = getRandomString(strrandom);
     Log.err(shit + ": " + e);
 }
